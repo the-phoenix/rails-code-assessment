dd72ba42d66ee79944f4c0201ba47ca8725ef999
the-phoenix <9201924+the-phoenix@users.noreply.github.com>
2020-06-04 05:33:32 +0800

    refactor routes a bit

--- config/routes.rb
+++ config/routes.rb
@@ -2,18 +2,15 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # resources :users, only: [:index, :create]
 
-  get 'signup', to: 'users#new'
+  get 'signup', to: 'users#new', as: 'signup'
   post 'signup', to: 'users#create'
 
-  get 'welcome', to: 'sessions#welcome'
-
   get 'login', to: 'sessions#new', as: 'login'
-
   post 'login', to: 'sessions#create'
 
   get 'logout', to: 'sessions#logout'
-
   get 'authorized', to: 'sessions#page_requires_login'
 
+  get 'welcome', to: 'sessions#welcome', as: 'welcome'
   root 'sessions#welcome'
 end
